bd457aafd56ea5fb4c65b67e379bc122d879fade
MatiasValansi <valansimatias@gmail.com>
2025-07-09 18:42:25 -0300

    Comencé a implementar el Front. Componente TaskForm creado con React funcionando, sin fetch

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import { TaskForm } from './components/TaskForm.jsx'
+import './App.css'
+
+function App() {
+
+    
+  return (
+    <>
+    <div className='App'>
+
+    <TaskForm></TaskForm>
+    </div>
+    
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontEnd/src/components/TaskForm.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+export const TaskForm = () => {
+
+    const [ newTaskName, setNewTaskName ] = useState()
+
+    const handleSubmit = (task) => {
+    task.preventDefault() //Para que no se recargue la página
+    alert(`Se añadió la tarea: ${newTaskName} con exito`)
+    localStorage.setItem("tasks", newTaskName)
+    setNewTaskName('')
+  }
+
+    return (
+        <form onSubmit={handleSubmit}>
+      <div>
+        <input type="text" placeholder='Ingresá una nueva tarea' onChange={(taskText) => setNewTaskName(taskText.target.value)}/>
+      
+        <button>Guardar Tarea</button>
+
+      </div>      
+    </form>
+    )
+}
